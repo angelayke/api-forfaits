133dc1f76e8caa4ec9dc2374d4cf8c924e29f3d4
angelayke <angebee2605@aol.com>
2023-01-24 20:54:50 -0500

    first commit

---
+++ modeles/forfaits.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "./include/config.php";
+
+class modele_produit {
+    public $id; 
+    public $nom;
+    public $description;
+    public $code;
+    public $categories;
+    public $etablissement;
+    public $date_debut;
+    public $date_fin;
+    public $prix;
+    public $nouveau_prix;
+    public $premium;
+    public $avis;
+
+
+}
+?>
---
+++ vue/forfaits/forfait.php
@@ -0,0 +1,7 @@
+<div class="container">
+  <div>
+    <h3><b>Nom du forfait: <?= $forfait->nom ?></b></h3>
+    <em><?= $forfait->description ?></em><br>
+    <h4>Prix: <strong><?= $forfait->prix ?>$</strong></h4>
+  </div>
+</div>
